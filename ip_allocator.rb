781db81b40e00049ab9bfd99896e89d0e4a3ed4a
Garry Dolley <gdolley@arpnetworks.com>
2009-01-14 00:30:03 -0800

    The first allocator method has been implemented and specs written.
    The specs pass and the allocator is unoptimized.  Seems to be correct so far.

--- ip_allocator.rb
+++ ip_allocator.rb
@@ -1,10 +1,14 @@
-require 'ipaddr'
+require 'rubygems'
+require 'netaddr'
+
+# All CIDR objects belong to the NetAddr module.  Where CIDR is referenced,
+# the complete object path is NetAddr::CIDR.
 
 class IPAllocator
-  # supernet: a IPAddr object describing a block of IPs from which allocations
+  # supernet: a CIDR object describing a block of IPs from which allocations
   #           can be made.
   #
-  # assignments: an array of IPAddr objects defining which IP blocks are 
+  # assignments: an array of CIDR objects defining which IP blocks are 
   #              already allocated and not eligible for assignment.
   #
   #              Naturally, all assignments should be a proper subset of 
@@ -12,13 +16,54 @@ class IPAllocator
   #
   # Example:
   #
-  #   IPAllocator.new(IPAddr.new('208.79.88.0/21'), [IPAddr.new('208.79.89.0/25'),
-  #                                                  IPAddr.new('208.79.91.0/24')])
-  def initialize(supernet, assignments)
+  #   @supernet  = NetAddr::CIDR.create('208.79.88.0/21')
+  #
+  #   @allocated = [NetAddr::CIDR.create('208.79.89.0/25'),
+  #                 NetAddr::CIDR.create('208.79.91.0/24')]
+  #
+  #   @allocator = IPAllocator.new(@supernet, @allocated)
+  def initialize(supernet, allocated)
+    @supernet  = supernet
+    @allocated = allocated
   end
 
-  # TODO: re-word this to be more clear
-  # Returns an IPAddr object of the first unused block of length 'size'
-  def first_unused(size)
+  # Synopsis:
+  #
+  #   Find the first unused block, per RFC 3531
+  #
+  # Arguments:
+  #
+  #   * size of the block to find, expressed in number of bits in the netmask.
+  #     For example::
+  #
+  #       24 = 256 IP block
+  #       25 = 128 IP block
+  #       ...
+  #       30 = 4 IP block
+  #
+  #   * options to pass to CIDR#allocate_rfc3531().
+  #       
+  # Returns:
+  #
+  #   NetAddr::CIDR object of first unused block found.  False otherwise.
+  def first_unused(size, opts = {})
+    if size < @supernet.bits
+      return false
+    end
+
+    defaults = { :Objectify => true,
+                 :Strategy  => :centermost }
+
+    @candidates = @supernet.allocate_rfc3531(size, defaults.merge(opts))
+
+    @available = @candidates.select do |candidate_block|
+      @allocated.each do |allocated_block|
+        if candidate_block.contains?(allocated_block)
+          break false
+        end
+      end
+    end
+
+    @available.first
   end
 end
--- ip_allocator_spec.rb
+++ ip_allocator_spec.rb
@@ -1,20 +1,30 @@
 require File.dirname(__FILE__) + '/ip_allocator'
+require 'ruby-debug'
 
 describe IPAllocator do
   before do
-    @supernet = IPAddr.new('208.79.88.0/21')
+    @supernet  = NetAddr::CIDR.create('208.79.88.0/21')
 
-    @assignments = [IPAddr.new('208.79.89.0/25'), 
-                    IPAddr.new('208.79.91.0/24')]
+    @allocated = [NetAddr::CIDR.create('208.79.89.0/25'), 
+                  NetAddr::CIDR.create('208.79.91.0/24')]
 
-    @ip_allocator = IPAllocator.new(@supernet, @assignments)
+    @allocator = IPAllocator.new(@supernet, @allocated)
   end
 
-  it "should find 208.79.92.0/23 as first unused /23 block" do
-    pending
+  describe "Allocations" do
+    it "should find 208.79.92.0/23 as first unused /23 block" do
+      @allocator.first_unused(23).should == '208.79.92.0/23'
+    end
+
+    it "should find 208.79.88.0/24 as first unused /24 block" do
+      @allocator.first_unused(24).should == '208.79.88.0/24'
+    end
   end
 
-  it "should find 208.79.88.0/24 as first unused /24 block" do
-    pending
+  describe "Edge Cases" do
+    it "should not find a block larger than the supernet" do
+      # /20 is larger than supernet of /21
+      @allocator.first_unused(20).should be_false
+    end
   end
 end
